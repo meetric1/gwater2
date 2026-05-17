11475b2f9e6f8c05fa88f2ba4586eba83b434e85
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-17 18:48:26 +0300

    Update gwater2_emitter.lua

--- lua/entities/gwater2_emitter.lua
+++ lua/entities/gwater2_emitter.lua
@@ -110,8 +110,8 @@ end
 function ENT:Draw(flags)
 	self:DrawModel(flags)
 
-	local isDepthPass = ( bit.band( flags, STUDIO_SSAODEPTHTEXTURE ) != 0 || bit.band( flags, STUDIO_SHADOWDEPTHTEXTURE ) != 0 )
-	if isDepthPass then return end
+	local is_depth_pass = ( bit.band( flags, STUDIO_SSAODEPTHTEXTURE ) != 0 or bit.band( flags, STUDIO_SHADOWDEPTHTEXTURE ) != 0 )
+	if is_depth_pass then return end
 
 	local pos, ang = self:GetPos(), self:GetAngles()
 	ang:RotateAroundAxis(ang:Up(), 180)
